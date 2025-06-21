26f7157b96da9ac3334d8d2c47f3b7133045a718
PJO Software <you@example.com>
2025-06-21 10:05:29 -0700

    only testing interpolation

--- scripts/0004_SecureSSH.sh
+++ scripts/0004_SecureSSH.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 read -p "Enter SSH port number desired (e.g., 1234): " port
 
-sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/' /etc/ssh/sshd_config
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+#sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+#sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/' /etc/ssh/sshd_config
+#sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 sed -i 's/#Port 22/Port $port/' /etc/ssh/sshd_config
 
 # cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
