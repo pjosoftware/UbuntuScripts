41ba3fa48a3ba508e551c72fa015810344056563
PJO Software <you@example.com>
2025-06-21 15:39:58 -0700

    Updated run all script

--- scripts/0000_RunAll.sh
+++ scripts/0000_RunAll.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-printf "\033[?1049h"
-clear
-tput civis
-
+bar_width=40
 scripts=()
 for script in *.sh; do
   [[ "$script" != "0000_RunAll.sh" ]] && scripts+=("$script")
@@ -11,39 +7,38 @@ done
 
 total=${#scripts[@]}
 count=0
-bar_width=40
 
-printf "\033[1;1H\033[0;36mProgress: [%-${bar_width}s]   0%%\033[0m" "$(printf -- '-%.0s' $(seq 1 $bar_width))"
-printf "\033[2;1H\033[0;36m%s\033[0m\n" "$(printf '─%.0s' $(seq 1 $(tput cols)))"
+printf "\033[?1049h"
+clear
+tput civis
+
+print_bar() {
+  local percent=$1
+  local script_name=$2
+  local filled=$((percent * bar_width / 100))
+  local empty=$((bar_width - filled))
+  local bar=$(printf "%0.s#" $(seq 1 $filled))
+  bar+=$(printf "%0.s-" $(seq 1 $empty))
+  printf "\033[1;1H\033[0;36mProgress: [%-${bar_width}s] %3d%% - Running: %-40s\033[0m" "$bar" "$percent" "$script_name" >&2
+  printf "\033[2;1H\033[0;36m%s\033[0m\n" "$(printf '─%.0s' $(seq 1 $(tput cols)))" >&2
+}
 
 for script in "${scripts[@]}"; do
   ((count++))
   percent=$((count * 100 / total))
-  filled=$((percent * bar_width / 100))
-  empty=$((bar_width - filled))
-  bar=$(printf "%0.s#" $(seq 1 $filled))
-  bar+=$(printf "%0.s-" $(seq 1 $empty))
-
-  printf "\033[1;1H\033[0;36mProgress: [%-${bar_width}s] %3d%% - Running: %-40s\033[0m" "$bar" "$percent" "$script"
-
-  tmpfile=$(mktemp)
+  print_bar "$percent" "$script"
 
   if grep -q "read " "$script"; then
     tput cnorm
   fi
 
-  { bash "$script"; echo "[$script finished with exit code $?]"; } | tee "$tmpfile" | sed "s/^/\033[3;1H/" 
-
-  tput civis
-
   tput cup 3 0
-  cat "$tmpfile"
-  rm -f "$tmpfile"
+  bash "$script"
+  tput civis
 
   echo ""
 done
 
-# Cleanup
 tput cnorm
 printf "\033[?1049l"
 echo -e "\033[0;32mAll scripts executed!\033[0m"
