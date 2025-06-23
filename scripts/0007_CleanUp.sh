6e79d1750faecfbee6500bfa78568fbaeb4147a8
PJO Software <you@example.com>
2025-06-22 21:07:31 -0700

    Keeping snap

--- scripts/0007_CleanUp.sh
+++ scripts/0007_CleanUp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt purge --auto-remove telnetd ftp vsftpd samba nfs-kernel-server nfs-common apache2 cups avahi-daemon rpcbind xinetd snapd whoopsie modemmanager nis yp-tools tftpd atftpd tftpd-hpa rsh-server rsh-redone-server
+apt purge --auto-remove telnetd ftp vsftpd samba nfs-kernel-server nfs-common apache2 cups avahi-daemon rpcbind xinetd whoopsie modemmanager nis yp-tools tftpd atftpd tftpd-hpa rsh-server rsh-redone-server
 apt autoremove
 apt clean
